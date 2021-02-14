7e7e663c7026031d81ed77f82762da1582893dd7
Negodin Vladislav <kol060k@yandex.ru>
2021-02-14 16:04:11 +0300

    Added messages

--- test_bot.py
+++ test_bot.py
@@ -1,5 +1,8 @@
 import telebot
 import os
+import re
+
+import strings.py # Немного полезных строк, используемых в коде
 
 PORT = int(os.environ.get('PORT', 5000))
 TOKEN = '1686208190:AAFhWf0SMuGXHTTOP9C90CIeml9cKtPeEWo'
@@ -9,11 +12,14 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+    bot.send_message(message.from_user.id, 'Я обучен преобразовывать картинки с применением к ним определённого стиля (NST), а также парочке мемов, на которые у моего создателя хватило фантазии и испорченности. Наслаждайтесь :)')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Привет!')
+    elif re.findall(forbidden_strings, message.text.lower()):
+        bot.send_message(message.from_user.id, 'Мат в боте запрещён!')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
